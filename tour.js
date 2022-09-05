6ef0081bf1e0099c56e42d170867ca584899b11f
tu10ng <2059734099@qq.com>
2022-09-05 17:32:44 +0800

    add route draw

--- tour.js
+++ tour.js
@@ -4,6 +4,8 @@ const ctx = canvas.getContext('2d');
 const building = {
     dormitory: [0,0,100,100],
     building3: [100,200,100,100],
+    building4: [400,400,100,100],
+    building5: [200,0,100,100],
 }    
 const width = canvas.width;
 const height = canvas.height;
@@ -18,7 +20,7 @@ function clear(){
     //ctx.drawImage(map_bg, 0, 0);
 }
 
-function draw(){
+function draw_building(){
     for (const [key,value] of Object.entries(building)){
         ctx.strokeRect(value[0], value[1], value[2], value[3]);
         
@@ -27,6 +29,23 @@ function draw(){
     }
 }
 
+function draw_route(){
+    for (let i = 0; i < route.length - 1; i++){
+        // route[i] -> route[i+1]
+        let first = building[route[i]];
+        let second = building[route[i+1]];
+        let start = [first[0] + first[2]/2, first[1] + first[3]/2];
+        let end = [second[0] + second[2]/2, second[1] + second[3]/2];
+        ctx.beginPath();
+        ctx.moveTo(start[0], start[1]);
+        ctx.lineTo(end[0], end[1]);
+        ctx.stroke();
+    }
+}
+function draw(){
+    draw_building();
+    draw_route();
+}
 function init(){
     clear();
     draw();
@@ -39,7 +58,7 @@ function init(){
             } else {
                 route.push(tmp_build);
             }
-            alert(route);
+            //alert(route);
             calculate();
             clear();
             draw();
