ff77e97b7d3149b43f5b5488d08e83acbb64419e
tu10ng <2059734099@qq.com>
2022-09-07 14:10:46 +0800

    add algo selection, mst algo; fix click listener

--- tour.js
+++ tour.js
@@ -1,4 +1,6 @@
 const canvas = document.getElementById('canvas');
+const tt = document.getElementById('tt');
+const select = document.getElementById('algo');
 const ctx = canvas.getContext('2d');
 
 const building = {
@@ -19,10 +21,68 @@ const map_bg = new Image();
 map_bg.src = "./map_bg.png";
 let route = [];
 let dist = {};
+// naive, salesman, mst, none
+let algorithm = select.value;
+
+function init(){
+    clear();
+    draw();
+    init_dist();
+    
+    canvas.addEventListener('click', (e) => {
+        let tmp_build = isBuilding(e);
+        if(tmp_build != false){
+            //console.log(tmp_build);
+            if (route.includes(tmp_build)){
+                route.splice(route.findIndex(i => i === tmp_build), 1)
+            } else {
+                route.push(tmp_build);
+            }
+            if (route.length != 0){
+                algo();
+            }
+            update();
+            clear();
+            draw();
+        }else{
+            console.log("not building");
+        }
+    });
+
+    select.addEventListener('change', () => {
+        algorithm = select.value;
+        if (route.length != 0){
+            algo();
+        }
+        update();
+        clear();
+        draw();
+    });
+}    
+
+function update(){
+    // 三教到宿舍到距离需要5分钟
+    // 50pixel <-> 1 min
+    let dis = 0;
+    for (let i = 0; i < route.length; i++){
+        if (i != route.length-1){
+            dis += dist[route[i]][route[i+1]];
+        }else{
+            dis += dist[route[i]][route[0]];
+        }
+    }
+    let time = Math.round(dis/56.6);
+    tt.innerHTML = time;
+}
 
 function clear(){
     ctx.clearRect(0, 0, canvas.width, canvas.height);    
     //ctx.drawImage(map_bg, 0, 0);
+}
+
+function draw(){
+    draw_building();
+    draw_route();
 }
 
 function draw_building(){
@@ -47,47 +107,21 @@ function draw_route(){
         let start = [first[0] + first[2]/2, first[1] + first[3]/2];
         let end = [second[0] + second[2]/2, second[1] + second[3]/2];
         
-        let headlen = 10; // length of head in pixels
+        let arrow_len = 30; // length of head in pixels
         var dx = end[0] - start[0];
         var dy = end[1] - start[1];
         var angle = Math.atan2(dy, dx);
         ctx.beginPath();
         ctx.moveTo(start[0], start[1]);
         ctx.lineTo(end[0], end[1]);
-        ctx.lineTo(end[0] - headlen * Math.cos(angle - Math.PI / 6), end[1] - headlen * Math.sin(angle - Math.PI / 6));
+        ctx.lineTo(end[0] - arrow_len * Math.cos(angle - Math.PI / 6), end[1] - arrow_len * Math.sin(angle - Math.PI / 6));
         ctx.moveTo(end[0], end[1]);
-        ctx.lineTo(end[0] - headlen * Math.cos(angle + Math.PI / 6), end[1] - headlen * Math.sin(angle + Math.PI / 6));        
+        ctx.lineTo(end[0] - arrow_len * Math.cos(angle + Math.PI / 6), end[1] - arrow_len * Math.sin(angle + Math.PI / 6));        
         ctx.stroke();
     }
 }
-function draw(){
-    draw_building();
-    draw_route();
-}
-function init(){
-    clear();
-    draw();
-    init_dist();
-    
-    canvas.addEventListener('click', (e) => {
-        let tmp_build = isBuilding(e);
-        if(tmp_build != false){
-            //console.log(tmp_build);
-            if (route.includes(tmp_build)){
-                route.splice(route.findIndex(i => i === tmp_build), 1)
-            } else {
-                route.push(tmp_build);
-            }
-            cal();
-            clear();
-            draw();
-        }else{
-            console.log("not building");
-        }
-    });
-}    
 
-function cal_dist(x1, y1, x2, y2){
+function algo_dist(x1, y1, x2, y2){
     let x = x2 - x1;
     let y = y2 - y1;
     return Math.sqrt(x * x + y * y);
@@ -98,7 +132,7 @@ function init_dist(){
     for (const [key, value] of Object.entries(building)){
         let tmp = {};
         for (const [k, v] of Object.entries(building)){
-            tmp[k] = cal_dist(value[0], value[1], v[0], v[1]);
+            tmp[k] = algo_dist(value[0], value[1], v[0], v[1]);
         }
         dist[key] = tmp;
     }
@@ -108,14 +142,31 @@ function init_dist(){
 
 
 // change `route' in place, route[0]->route[1]->route[2]->...->route[n-1]
-function cal(){
-    cal_naive();
-    //cal_salesman();
+// although called algorithm, its not pure function, but just calculation
+// with side effects
+function algo(){
+    switch(algorithm){
+    case 'naive':
+        algo_naive();
+        break;
+    case 'salesman':
+        algo_salesman();
+        break;
+    case 'mst':
+        algo_mst();
+        break;
+    case 'none':
+        break;
+    default:
+        algo_naive();
+        break;
+    }
 }
 
-function cal_naive(){
+function algo_naive(){
     let ret = 1000000000;
     let mem;
+
     for (const tmp of perms(route)){
         let time = 0;
         for (let i = 0; i < tmp.length; i++){
@@ -144,7 +195,65 @@ function perms(xs) {
     });
 }
 
-function cal_salesman(){
+function algo_mst(){
+    const tree = new Tree(route[0]);
+    let rest = route.slice();
+    rest.splice(0, 1);
+    for (let i = 0; i < route.length - 1; i++){
+        let from;
+        let to;
+        let min = Number.MAX_VALUE;
+        for (const node of tree.preOrder()){
+            for (const r of rest){
+                if (dist[node.key][r] < min){
+                    from = node.key;
+                    to = r;
+                    min = dist[node.key][r];
+                }
+            }
+        }
+        tree.insert(from, to);
+        rest.splice(rest.findIndex(i => i === to), 1)
+    }
+    
+    route = [...tree.preOrder()].map(x => x.key);
+}
+
+class TreeNode {
+    constructor(key, parent = null) {
+        this.key = key;
+        this.parent = parent;
+        this.children = [];
+    }
+}
+
+class Tree{
+    constructor(key){
+        this.root = new TreeNode(key);
+    }
+    
+    insert(parent, key){
+        for (const node of this.preOrder()){
+            if (node.key === parent){
+                node.children.push(new TreeNode(key, node));
+                return true;
+            }
+        }
+        alert("should not return false");
+    }
+    
+    *preOrder(node = this.root){
+        yield node;
+        if (node.children.length){
+            for (const child of node.children){
+                yield* this.preOrder(child);
+            }
+        }
+    }
+    
+}
+    
+function algo_salesman(){
     let len = route.length;
     
     // init memo
@@ -173,7 +282,4 @@ function isBuilding(e){
     return false;
 }
 
-
-
 init();
-
